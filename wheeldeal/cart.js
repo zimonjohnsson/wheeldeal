5e34ac197440461e937ff5c4e7d9c3699f87a77f
Queereell <thelegendaryyouth@gmail.com>
2023-08-25 12:49:46 +0200

    NEED

--- wheeldeal/cart.js
+++ wheeldeal/cart.js
@@ -1,37 +1,31 @@
-let cart = {};
-if (localStorage.getItem("cart")) {
-    cart = JSON.parse(localStorage.getItem("cart"));
+function doShowAll() {
+    if (CheckBrowser()) {
+        var key = "";
+        var list = "<tr><th>Item</th><th>Value</th></tr>\n";
+        var i = 0;
+        //For a more advanced feature, you can set a cap on max items in the cart.
+        for (i = 0; i <= localStorage.length-1; i++) {
+            key = localStorage.key(i);
+            list += "<tr><td>" + key + "</td>\n<td>"
+                    + localStorage.getItem(key) + "</td></tr>\n";
+        }
+        //If no item exists in the cart.
+        if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+        }
+        //Bind the data to HTML table.
+        //You can use jQuery, too.
+        document.getElementById('list').innerHTML = list;
+    } else {
+        alert('Cannot save shopping list as your browser does not support HTML 5');
+    }
 }
 
-let tbody = document.getElementById("tbody");
-
-for (let id in cart) {
-    let item = cart[id];
-
-    let tr = document.createElement('tr')
-
-    let title_td = document.createElement('td')
-    title_td.textContent = item.title
-    tr.appendChild(title_td)
-
-
-    let price_td = document.createElement("td");
-    price_td.textContent = item.price;
-    tr.appendChild(price_td);
-
-    let qty_td = document.createElement("td");
-    qty_td.textContent = item.qty;
-    tr.appendChild(qty_td);
-
-    let sum_td=document.createElement("td");
-    sum_td.textContent= item.qty*item.price;
-    tr.appendChild(sum_td);
-
-    tbody.appendChild(tr)
-
-}
-
-function tabort(){
-    window.localStorage.clear();
-    location.reload();
+function CheckBrowser() {
+    if ('localStorage' in window && window['localStorage'] !== null) {
+        // We can use localStorage object to store data.
+        return true;
+    } else {
+            return false;
+    }
 }
