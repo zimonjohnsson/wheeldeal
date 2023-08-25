84a6dfb4da92620e7e63cb03ae9ad5a0a784d248
Queereell <thelegendaryyouth@gmail.com>
2023-08-25 15:07:06 +0200

    Update

--- wheeldeal/cart.js
+++ wheeldeal/cart.js
@@ -1,33 +1,25 @@
-
-function doShowAll() {
+function CheckBrowser() {
+    if ('localStorage' in window && window['localStorage'] !== null) {
+        return true;
+    }
+    else return false;
+}
+function showCart() {
     if (CheckBrowser()) {
-        var key = "";
-        var list = "<tr><th>Item</th><th>Value</th></tr>\n";
-        var i = 0;
-        for (i = 0; i <= localStorage.length-1; i++) {
-            key = localStorage.key(i);
-            list += "<tr><td>" + key + "</td>\n<td>"
-                    + localStorage.getItem(key) + "</td></tr>\n";
-        }
-        if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
-            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
-        }
-        document.getElementById('list').innerHTML = list;
+        
     } else {
         alert('Cannot save shopping list as your browser does not support HTML 5');
     }
 }
 
-function CheckBrowser() {
-    if ('localStorage' in window && window['localStorage'] !== null) {
-        // We can use localStorage object to store data.
-        return true;
-    } else {
-            return false;
-    }
+function addToCart() {
+
+}
+function removeFromCart() {
+
 }
 
 let Items = [
-    { Name: "", Price:  },
-    { Name: "",  }
+    { Name: "Retro Rattletron 3000", Price: 100, Category: "Bil" },
+    { Name: "Nightmare Flash", Price: 15, Category: "Cykel" }
 ]
