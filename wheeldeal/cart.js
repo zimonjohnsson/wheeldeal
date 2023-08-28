d15e7b553fe743d4421a1b23a90c77fe44d2b878
Queereell <thelegendaryyouth@gmail.com>
2023-08-28 10:09:54 +0200

    Update cart.js

--- wheeldeal/cart.js
+++ wheeldeal/cart.js
@@ -1,22 +1,24 @@
+localStorage.setItem(itemsList, null);
+localStorage.setItem(count, 0);
+localStorage.setItem(totalPrice, 0);
+
 function CheckBrowser() {
-    if ('localStorage' in window && window['localStorage'] !== null) {
-        return true;
-    }
-    else return false;
+	if ('localStorage' in window && window['localStorage'] !== null) {
+		return true;
+	}
+	else return false;
 }
 function showCart() {
-    if (CheckBrowser()) {
-        localStorage.setItem(itemsList, null);
-        localStorage.setItem(count, 0);
-        localStorage.setItem(totalPrice, 0);
-        
-    } else {
-        alert('Your browser does not support HTML 5');
-    }
+	if (CheckBrowser()) {
+
+		
+	} else {
+		alert('Your browser does not support HTML 5');
+	}
 }
 
 function addToCart(name) {
-    addedItems.push(name);
+	addedItems.push(name);
 
 }
 function removeFromCart() {
@@ -25,6 +27,6 @@ function removeFromCart() {
 
 let addedItems = [];
 let Items = [
-    { Name: "Retro Rattletron 3000", Price: 100, Category: "Bil" },
-    { Name: "Nightmare Flash", Price: 15, Category: "Cykel" }
+	{ Name: "Retro Rattletron 3000", Price: 100, Category: "Bil" },
+	{ Name: "Nightmare Flash", Price: 15, Category: "Cykel" }
 ]
