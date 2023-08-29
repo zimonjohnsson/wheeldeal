5b8889c283187ec1a5b8ffd20a196d1ab7abf626
Queereell <thelegendaryyouth@gmail.com>
2023-08-29 15:42:12 +0200

    DONE

--- wheeldeal/invoice.js
+++ wheeldeal/invoice.js
@@ -1,4 +1,8 @@
 
 function loadCart() {
-    document.getElementById("invoiceTable").innerHTML = "<tr> <td>Product 1</td> <td>2</td> <td>$100.00</td> </tr>";
+    var parseHTML = "<tr><th>Description</th><th>Quantity</th><th>Total</th></tr>";
+
+    parseHTML += "<tr> <td>Product 1</td> <td>2</td> <td>$100.00</td> </tr>";
+
+    document.getElementById("invList").innerHTML = parseHTML;
 }
